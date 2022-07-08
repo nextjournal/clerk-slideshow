39d2424a66f93ab2db8957c6f0b535bc1dc539db
Martin Kavalar <martin@nextjournal.com>
2022-07-08 13:57:13 +0200

    Bump Clerk and use eval-cljs-str to move viewer code to cljs file

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+((clojure-mode (cider-clojure-cli-aliases . ":dev:nextjournal/clerk")
+               (cider-preferred-build-tool . clojure-cli)))
--- src/nextjournal/clerk_slideshow.clj
+++ src/nextjournal/clerk_slideshow.clj
@@ -1,9 +1,10 @@
 ;; # 🎠 Clerk Slideshow
 ^{:nextjournal.clerk/visibility :hide-ns}
 (ns nextjournal.clerk-slideshow
-  (:require [nextjournal.clerk :as clerk]
-            [nextjournal.clerk.viewer :as v]
-            [clojure.walk :as w]))
+  (:require [clojure.java.io :as io]
+            [clojure.walk :as w]
+            [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.viewer :as v]))
 
 ;; With a custom viewer and some helper functions, we can show Clerk notebooks
 ;; as slideshow. `->slide` wraps a collection of blocks into markup suitable
@@ -20,11 +21,15 @@
 ;; partitions its blocks into slides separated by Markdown rulers (`---`).
 (defn doc->slides [{:as doc :keys [blocks]}]
   (sequence (comp (mapcat (partial v/with-block-viewer doc))
-              (mapcat #(if (= :markdown (v/->viewer %)) (-> % v/->value :content) [(v/->value %)]))
-              (partition-by (comp #{:ruler} :type))
-              (remove (comp #{:ruler} :type first))
-              (map ->slide))
-    blocks))
+                  (mapcat #(if (= :markdown (v/->viewer %)) (-> % v/->value :content) [(v/->value %)]))
+                  (partition-by (comp #{:ruler} :type))
+                  (remove (comp #{:ruler} :type first))
+                  (map ->slide))
+            blocks))
+
+^::clerk/no-cache
+(clerk/eval-cljs-str
+ (slurp (io/resource "nextjournal/clerk_slideshow/viewer.cljs")))
 
 ;; We can then override Clerk’s default notebook viewer with
 ;; a custom slideshow viewer which can be required and set using
@@ -32,64 +37,6 @@
 (def viewer
   {:name :clerk/notebook
    :transform-fn (comp v/mark-presented
-                   (v/update-val (comp (partial w/postwalk (v/when-wrapped v/inspect-wrapped-value))
-                                   doc->slides)))
-   :render-fn '(fn [slides]
-                 (defonce !state (reagent/atom {:current-slide 0
-                                                :grid? false
-                                                :viewport-width js/innerWidth
-                                                :viewport-height js/innerHeight}))
-                 (v/html
-                   (reagent/with-let [ref-fn (fn [el]
-                                               (when el
-                                                 (swap! !state assoc :stage-el el)
-                                                 (js/addEventListener "resize"
-                                                   #(swap! !state assoc
-                                                      :viewport-width js/innerWidth
-                                                      :viewport-height js/innerHeight))
-                                                 (js/document.addEventListener "keydown"
-                                                   (fn [e]
-                                                     (case (.-key e)
-                                                       "Escape" (swap! !state update :grid? not)
-                                                       "ArrowRight" (when-not (:grid? !state)
-                                                                      (swap! !state update :current-slide #(min (dec (count slides)) (inc %))))
-                                                       "ArrowLeft" (when-not (:grid? !state)
-                                                                     (swap! !state update :current-slide #(max 0 (dec %))))
-                                                       nil)))))
-                                      default-transition {:type :spring :duration 0.4 :bounce 0.1}]
-                     (let [{:keys [grid? current-slide viewport-width viewport-height]} @!state]
-                       [:div.overflow-hidden.relative.bg-slate-50
-                        {:ref ref-fn :id "stage" :style {:width viewport-width :height viewport-height}}
-                        (into [:> (.. framer-motion -motion -div)
-                               {:style {:width (if grid? viewport-width (* (count slides) viewport-width))}
-                                :initial false
-                                :animate {:x (if grid? 0 (* -1 current-slide viewport-width))}
-                                :transition default-transition}]
-                          (map-indexed
-                            (fn [i slide]
-                              (let [width 250
-                                    height 150
-                                    gap 40
-                                    slides-per-row (int (/ viewport-width (+ gap width)))
-                                    col (mod i slides-per-row)
-                                    row (int (/ i slides-per-row))]
-                                [:> (.. framer-motion -motion -div)
-                                 {:initial false
-                                  :class ["absolute left-0 top-0 overflow-x-hidden bg-white"
-                                          (when grid?
-                                            "rounded-lg shadow-lg overflow-y-hidden cursor-pointer ring-1 ring-slate-200 hover:ring hover:ring-blue-500/50 active:ring-blue-500")]
-                                  :animate {:width (if grid? width viewport-width)
-                                            :height (if grid? height viewport-height)
-                                            :x (if grid? (+ gap (* (+ gap width) col)) (* i viewport-width))
-                                            :y (if grid? (+ gap (* (+ gap height) row)) 0)}
-                                  :transition default-transition
-                                  :on-click #(when grid? (swap! !state assoc :current-slide i :grid? false))}
-                                 [:> (.. framer-motion -motion -div)
-                                  {:style {:width viewport-width
-                                           :height viewport-height
-                                           :transformOrigin "left top"}
-                                   :initial false
-                                   :animate {:scale (if grid? (/ width viewport-width) 1)}
-                                   :transition default-transition}
-                                  slide]]))
-                            slides))]))))})
+                       (v/update-val (comp (partial w/postwalk (v/when-wrapped v/inspect-wrapped-value))
+                                           doc->slides)))
+   :render-fn 'nextjournal.clerk-slideshow-viewer/render-slides})
